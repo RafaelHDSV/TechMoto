e2b473cb5e7c2e12916baee900f29f5516a23761
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-05-22 23:31:41 -0300

    refactor: rename solution name from telas to TechMoto

---
+++ TechMoto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechMoto
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using (frmLogin loginForm = new frmLogin())
+            {
+                if (loginForm.ShowDialog() == DialogResult.OK)
+                {
+                    var usuarioLogado = loginForm.UsuarioLogado;
+                    Application.Run(new frmMain(usuarioLogado));
+                }
+            }
+        }
+    }
+}
